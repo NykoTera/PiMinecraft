510692e6dc4120007bb0fe76d4f368402960bfcb
NykoTera <44168242+NykoTera@users.noreply.github.com>
2019-10-09 16:00:08 +0200

    Update updatespigot.sh

--- updatespigot.sh
+++ updatespigot.sh
@@ -22,9 +22,8 @@ sudo java -Xmx1024M -jar chemSpigot/BuildTools.jar --rev 1.14.4
 sudo tar -cvzf chemSave/logsave/logsave-$(date +%Y%m%d-%H%M).tar.gz chemServ/logs
 sudo rm chemServ/spigot.jar chemServ/logs -r
 
-cd chemServ
 #   Deleting older logs and copying latest spigot.jar
-ls -1rt chemSave/logsave | head -n-2 | xargs -I TITI sudo rm chemSave/logsave/TITI
+find chemSave/logsave -type f -exec basename {} \; | sort -r | head -n-2 | xargs -I TITI sudo rm chemSave/logsave/TITI
 cp chemSpigot/spigot-* chemServ/spigot.jar
 
 #   Restart Minecraft server
@@ -32,8 +31,6 @@ sudo systemctl start serverServ.service
 sleep 10
 
 #   Conservation et upload sur la dropbox des 2 dernières sauvegardes
-ls -1rt chemSave | head -n-2 | xargs -I save rm chemSave/save
+find chemSave -type f -name '*tar*' -exec basename {} \; | sort -r | head -n-2 | xargs -I save rm chemSave/save
 $pathUpload/dropbox_uploader.sh -q -d list / | awk "{print \$3}" | xargs -I TATA $pathUpload/dropbox_uploader.sh -d delete TATA
-ls -1rt chemSave | xargs -I TUTU $pathUpload/dropbox_uploader.sh -d upload chemSave/TUTU TUTU
-
-cd
+find chemSave -type f -name '*tar*' -exec basename {} \; | sort | xargs -I TUTU $pathUpload/dropbox_uploader.sh -d upload chemSave/TUTU TUTU
